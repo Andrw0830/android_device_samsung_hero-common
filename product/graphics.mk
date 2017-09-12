28a9c8f77364c358062d87e59e806c8be45e65c2
Andrw0830 <Andrw0830@gmail.com>
2017-09-12 17:26:57 -0400

    Put correct CPU value, update graphics and settings from new tree
    Change-Id: I9d0243fd307b03b54b54cc49c46bb5d3cd7b2b97

--- product/graphics.mk
+++ product/graphics.mk
@@ -11,13 +11,10 @@ PRODUCT_COPY_FILES += \
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.bq.gpu_to_cpu_unsupported=1 \
-	ro.opengles.version=196609 \
-	debug.hwc.force_gpu=1 \
-	ro.bq.gpu_to_cpu_unsupported=1 \
-	ro.opengles.version=196609 \
-        ro.sf.lcd_density=640
+	ro.opengles.version=196610 \
+	ro.sf.lcd_density=640 \
+	debug.hwc.force_gpu=1
 
 # Properties - HWC - not used on cm/aosp
 PRODUCT_PROPERTY_OVERRIDES += \
-	debug.hwc.winupdate=1 \
-	debug.hwc.otf=1
+	debug.hwc.winupdate=1
